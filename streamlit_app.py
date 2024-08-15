f9d02ed32eeb3c8abd4a86dfd070e3f1257945ba
Lng Thnk <inothaveidea@gmail.com>
2024-08-15 15:55:38 +0700

    remove indent

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,136 +16,135 @@ This is a minimal, reproducible example of how to scrape the web with Selenium a
 Fork this repo, and edit `/streamlit_app.py` to customize this app to your heart's desire. :heart:
 """
 
-with st.echo():
-    from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.chrome.service import Service
-    from webdriver_manager.chrome import ChromeDriverManager
-    from webdriver_manager.core.os_manager import ChromeType
 
-    @st.cache_resource
-    def get_driver():
-        return webdriver.Chrome(
-            service=Service(
-                ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-            ),
-            options=options,
-        )
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
+
+@st.cache_resource
+def get_driver():
+    return webdriver.Chrome(
+        service=Service(
+            ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+        ),
+        options=options,
+    )
 
 
-    def check_TRI_price():
-        #driver = get_driver()
-        url = 'https://www.set.or.th/en/market/index/tri/overview'
-        driver.get(url)
+def check_TRI_price():
+    #driver = get_driver()
+    url = 'https://www.set.or.th/en/market/index/tri/overview'
+    driver.get(url)
 
-        #SET50 TRI
-        index_xpath = '/html/body/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div/div/div/div[2]/div[2]/table/tbody/tr[2]/td[2]'
-        index = driver.find_element(By.XPATH, index_xpath)
-        update = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div/div/div/div[1]/span')
+    #SET50 TRI
+    index_xpath = '/html/body/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div/div/div/div[2]/div[2]/table/tbody/tr[2]/td[2]'
+    index = driver.find_element(By.XPATH, index_xpath)
+    update = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div/div/div/div[1]/span')
 
-        index_float = float(index.text.replace(",",""))
+    index_float = float(index.text.replace(",",""))
 
-        TRI = {'index':index_float, 'update':update.text[6:]}
-        #driver.quit()
-        return TRI
-    
-    def check_SET50_price():
-        #driver = get_driver()
-        url = 'https://www.set.or.th/en/market/index/set50/overview'
-        driver.get(url)
+    TRI = {'index':index_float, 'update':update.text[6:]}
+    #driver.quit()
+    return TRI
 
-        index_xpath = '/html/body/div[1]/div/div/div[2]/div/div[1]/div[2]/div[2]/div[1]/div[2]/div[2]'
-        update_xpath = '/html/body/div[1]/div/div/div[2]/div/div[1]/div[3]/div/div[1]/div[2]/span'
-        index = driver.find_element(By.XPATH, index_xpath)
-        index_update = driver.find_element(By.XPATH, update_xpath)
+def check_SET50_price():
+    #driver = get_driver()
+    url = 'https://www.set.or.th/en/market/index/set50/overview'
+    driver.get(url)
 
-        index_float = float(index.text.replace(",",""))
+    index_xpath = '/html/body/div[1]/div/div/div[2]/div/div[1]/div[2]/div[2]/div[1]/div[2]/div[2]'
+    update_xpath = '/html/body/div[1]/div/div/div[2]/div/div[1]/div[3]/div/div[1]/div[2]/span'
+    index = driver.find_element(By.XPATH, index_xpath)
+    index_update = driver.find_element(By.XPATH, update_xpath)
 
-        SET50 = {'index':index_float, 'update': index_update.text}
+    index_float = float(index.text.replace(",",""))
 
-        #driver.quit()
+    SET50 = {'index':index_float, 'update': index_update.text}
 
-        return SET50
-    
-    def dl_set50(SET50_data):
-        TRI = check_TRI_price()
-        SET50 = check_SET50_price()
-        check_TRI_date = datetime.strptime(TRI['update'], '%d %b %Y')
-        check_data_date = datetime.strptime(SET50_data.index[-1], '%d/%m/%Y')
+    #driver.quit()
 
-        if check_TRI_date > check_data_date:
-            df_tri = pd.DataFrame({'SET50':[SET50['index']], 'SET50_TRI':[TRI['index']]}, index = [check_TRI_date.strftime("%d/%m/%Y")])
-            df_tri.index.name = 'DATE'
-            df_tri2 = pd.concat([SET50_data,df_tri])
-        else:
-            st.write('data is up to date')
-        return df_tri2
+    return SET50
 
-    def del_time(df_to_del):
-        date_str = []
-        for datee in list(df_to_del.index):
-        # Convert to Pandas datetime object
-            date_time_obj = pd.to_datetime(datee)
+def dl_set50(SET50_data):
+    TRI = check_TRI_price()
+    SET50 = check_SET50_price()
+    check_TRI_date = datetime.strptime(TRI['update'], '%d %b %Y')
+    check_data_date = datetime.strptime(SET50_data.index[-1], '%d/%m/%Y')
 
-            # Remove time from the datetime object
-            date_var = date_time_obj.date()
-
-            # Convert the date object to a string
-            date_str.append(date_var.strftime('%Y-%m-%d'))
-    
-        df_to_del.index = date_str
-        return df_to_del
-
-    def download_pricedata(old_price_data):
-        old_price_data = del_time(old_price_data)
-        tic_list = old_price_data.columns.to_list()
-        SET_MAI = []
-        for ticker in tic_list:
-            SET_MAI.append(ticker + ".BK")
-        #get lastest date
-        lastest_date = old_price_data.index[-1]
-        lastest_date = datetime.strptime(lastest_date, '%Y-%m-%d')+ timedelta(days = 1)
-        lastest_date = lastest_date.strftime('%Y-%m-%d')
-        present_date = datetime.today().strftime('%Y-%m-%d')
-        #download lastest to present date
-        price_data2 = yf.download(SET_MAI, start=lastest_date, end=present_date )['Close']
-        price_data2.columns = [col[:-3] for col in price_data2.columns]
-        #combine
-        price_data4 = del_time(old_price_data)
-        price_data5 = del_time(price_data2)
-        price_data3 = pd.concat([price_data4, price_data5])
-        #price_data3 = price_data3.drop_duplicates(subset='Date')
-        price_data3.index.name = 'Date'
-
-        return price_data3
-    
-    
-    uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-    if len(uploaded_files) == 2:
-
-        if uploaded_files[0].name == 'SET50TRI_Close.csv':
-            SET50_data = pd.read_csv(uploaded_files[0], index_col ='DATE')
-            price_data = pd.read_csv(uploaded_files[1], index_col='Date')
-        elif uploaded_files[0].name == 'SET_MAI_Close.csv':
-            SET50_data = pd.read_csv(uploaded_files[0], index_col ='Date')
-            price_data = pd.read_csv(uploaded_files[1], index_col='DATE')
-
-        options = Options()
-        options.add_argument("--disable-gpu")
-        options.add_argument("--headless")
-        driver = get_driver()
-        try:
-            #df_tri = dl_set50(SET50_data)
-            df_tri = SET50_data
-        except:
-            st.write("please reboot")
-
-        if isinstance(df_tri, pd.DataFrame):
-            new_price_data = download_pricedata(price_data)
-
-            st.dataframe(df_tri)
-            st.dataframe(new_price_data)
+    if check_TRI_date > check_data_date:
+        df_tri = pd.DataFrame({'SET50':[SET50['index']], 'SET50_TRI':[TRI['index']]}, index = [check_TRI_date.strftime("%d/%m/%Y")])
+        df_tri.index.name = 'DATE'
+        df_tri2 = pd.concat([SET50_data,df_tri])
     else:
-        st.write('please upload corrected files')
-        
-    #pd.read_excel('https://www.set.or.th/dat/eod/listedcompany/static/listedCompanies_th_TH.xls', engine='calamine', skiprows=1)
+        st.write('data is up to date')
+    return df_tri2
+
+def del_time(df_to_del):
+    date_str = []
+    for datee in list(df_to_del.index):
+    # Convert to Pandas datetime object
+        date_time_obj = pd.to_datetime(datee)
+
+        # Remove time from the datetime object
+        date_var = date_time_obj.date()
+
+        # Convert the date object to a string
+        date_str.append(date_var.strftime('%Y-%m-%d'))
+
+    df_to_del.index = date_str
+    return df_to_del
+
+def download_pricedata(old_price_data):
+    old_price_data = del_time(old_price_data)
+    tic_list = old_price_data.columns.to_list()
+    SET_MAI = []
+    for ticker in tic_list:
+        SET_MAI.append(ticker + ".BK")
+    #get lastest date
+    lastest_date = old_price_data.index[-1]
+    lastest_date = datetime.strptime(lastest_date, '%Y-%m-%d')+ timedelta(days = 1)
+    lastest_date = lastest_date.strftime('%Y-%m-%d')
+    present_date = datetime.today().strftime('%Y-%m-%d')
+    #download lastest to present date
+    price_data2 = yf.download(SET_MAI, start=lastest_date, end=present_date )['Close']
+    price_data2.columns = [col[:-3] for col in price_data2.columns]
+    #combine
+    price_data4 = del_time(old_price_data)
+    price_data5 = del_time(price_data2)
+    price_data3 = pd.concat([price_data4, price_data5])
+    #price_data3 = price_data3.drop_duplicates(subset='Date')
+    price_data3.index.name = 'Date'
+
+    return price_data3
+
+
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+if len(uploaded_files) == 2:
+
+    if uploaded_files[0].name == 'SET50TRI_Close.csv':
+        SET50_data = pd.read_csv(uploaded_files[0], index_col ='DATE')
+        price_data = pd.read_csv(uploaded_files[1], index_col='Date')
+    elif uploaded_files[0].name == 'SET_MAI_Close.csv':
+        SET50_data = pd.read_csv(uploaded_files[0], index_col ='Date')
+        price_data = pd.read_csv(uploaded_files[1], index_col='DATE')
+
+    options = Options()
+    options.add_argument("--disable-gpu")
+    options.add_argument("--headless")
+    driver = get_driver()
+    try:
+        df_tri = dl_set50(SET50_data)
+    except:
+        st.write("please reboot")
+
+    if isinstance(df_tri, pd.DataFrame):
+        new_price_data = download_pricedata(price_data)
+
+        st.dataframe(df_tri)
+        st.dataframe(new_price_data)
+else:
+    st.write('please upload corrected files')
+    
+#pd.read_excel('https://www.set.or.th/dat/eod/listedcompany/static/listedCompanies_th_TH.xls', engine='calamine', skiprows=1)
